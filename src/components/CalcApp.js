129e9e3d2b040384fb1afde53d9ac33161feee3e
Serena Huang <serena1993719@gmail.com>
2016-11-23 02:13:13 +0800

    done

--- src/components/CalcApp.js
+++ src/components/CalcApp.js
@@ -6,12 +6,65 @@ class CalcApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // TODO
+      operator: '=',
+      pending: 0,
+      number: 0,
+      reset: false,
+    };
+    this.addDigit = this.addDigit.bind(this);
+    this.setOperator = this.setOperator.bind(this);
+  }
+
+  setOperator(operator) {
+    return () => {
+      if (this.state.reset) {
+        this.setState({
+          operator: operator,
+        });
+      } else {
+        if (this.state.operator === '=') {
+          if (operator !== '=') {
+            this.setState({
+              pending: this.state.number,
+              operator: operator,
+              reset: true,
+            });
+          }
+        } else {
+          let result = 0;
+          const {pending, number} = this.state;
+          if (this.state.operator === '+') { result = pending + number; }
+          else if (this.state.operator === '-') { result = pending - number; }
+          else if (this.state.operator === '*') { result = pending * number; }
+          else if (this.state.operator === '/') { result = pending / number; }
+          this.setState({
+            pending: result,
+            number: result,
+            operator: operator,
+            reset: true,
+          });
+        }
+      }
+    };
+  }
+
+  addDigit(digit) {
+    return () => {
+      const next = this.state.reset ? digit : this.state.number * 10 + digit;
+      this.setState({
+        number: next,
+        reset: false,
+      });
     };
   }
 
   resetState() {
-    // TODO
+    this.setState({
+      operator: '=',
+      pending: 0,
+      number: 0,
+      reset: false,
+    });
   }
 
   showNotImplemented() {
@@ -23,19 +76,36 @@ class CalcApp extends React.Component {
       <div className="calc-app">
         <div className="calc-container">
           <div className="calc-output">
-            <div className="calc-display">1980</div>
+            <div className="calc-display">{this.state.number}</div>
           </div>
           <div className="calc-row">
             <CalcButton onClick={this.resetState.bind(this)}>AC</CalcButton>
             <CalcButton onClick={this.showNotImplemented.bind(this)}>+/-</CalcButton>
             <CalcButton onClick={this.showNotImplemented.bind(this)}>%</CalcButton>
-            <CalcButton className="calc-operator">÷</CalcButton>
+            <CalcButton className="calc-operator" onClick={this.setOperator('/')}>÷</CalcButton>
           </div>
           <div className="calc-row">
-            <CalcButton className="calc-number">7</CalcButton>
-            <CalcButton className="calc-number"></CalcButton>
-            <CalcButton className="calc-number"></CalcButton>
-            <CalcButton className="calc-operator"></CalcButton>
+            <CalcButton className="calc-number" onClick={this.addDigit(7)}>7</CalcButton>
+            <CalcButton className="calc-number" onClick={this.addDigit(8)}>8</CalcButton>
+            <CalcButton className="calc-number" onClick={this.addDigit(9)}>9</CalcButton>
+            <CalcButton className="calc-operator" onClick={this.setOperator('*')}>x</CalcButton>
+          </div>
+          <div className="calc-row">
+            <CalcButton className="calc-number" onClick={this.addDigit(4)}>4</CalcButton>
+            <CalcButton className="calc-number" onClick={this.addDigit(5)}>5</CalcButton>
+            <CalcButton className="calc-number" onClick={this.addDigit(6)}>6</CalcButton>
+            <CalcButton className="calc-operator" onClick={this.setOperator('-')}>-</CalcButton>
+          </div>
+          <div className="calc-row">
+            <CalcButton className="calc-number" onClick={this.addDigit(1)}>1</CalcButton>
+            <CalcButton className="calc-number" onClick={this.addDigit(2)}>2</CalcButton>
+            <CalcButton className="calc-number" onClick={this.addDigit(3)}>3</CalcButton>
+            <CalcButton className="calc-operator" onClick={this.setOperator('+')}>+</CalcButton>
+          </div>
+          <div className="calc-row">
+            <CalcButton className="calc-number" onClick={this.addDigit(0)}>0</CalcButton>
+            <CalcButton className="calc-number" onClick={this.addDigit(9)}>.</CalcButton>
+            <CalcButton className="calc-operator" onClick={this.setOperator('=')}>=</CalcButton>
           </div>
         </div>
       </div>
